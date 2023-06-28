6120f00ec59e423b34834fb22c8dbd9d49fa47a0
groot737 <giorgiquchuloria7@gmail.com>
2023-06-28 19:56:51 +0400

    created map page files

---
+++ scripts/map.js
@@ -0,0 +1,12 @@
+var map = L.map('map', {
+    detectRetina: true
+}).setView([51.505, -0.09], 13);
+
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
